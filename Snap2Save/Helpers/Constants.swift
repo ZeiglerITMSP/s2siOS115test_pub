21e456ffc104159a2d80658c652d1e8a4a56424e
Kiran Sarella <kiran.sarella@appitventures.com>
2017-05-02 11:36:04 +0530

    fixed displaying both 'no offer exists message' and cached image in offers screen

--- Snap2Save/Helpers/Constants.swift
+++ Snap2Save/Helpers/Constants.swift
@@ -10,11 +10,12 @@ import Foundation
 import UIKit
 
 
-// development url
+// development url old
 //var hostUrl = "http://dev.snap2save-a-lot.com/api";
 // test url
 //var hostUrl = "http://dev.snap2save-a-lot.com/test/api"
 
+// new
 // test
 var hostUrl = "http://test-api.snap2save.com"
 // dev
